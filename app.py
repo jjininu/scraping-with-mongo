97fc5ce23445c83ed7eb148ec6d7e31d75ba5e9e
jjininu@gmail.com <jjininu@gmail.com>
2022-06-17 20:25:52 -0400

    update

--- app.py
+++ app.py
@@ -36,32 +36,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 import json
 
-from flask import Flask, render_template, request,jsonify
 
-import mongodb
-import scraping
-from scraping import *
-from mongodb import *
-
-app  = Flask(__name__)
-
-@ app.route('/',methods = ["GET","POST"])
-def home_page():
-    return render_template('home.html')
-
-
-@app.route('/result',methods = ["GET","POST"])
-def result():
-
-    price1 = request.form["content1"]
-    price2 = request.form["content2"]
-    price = {'$lte' : int(price2)}
-    input = {price1:price}
-
-
-    data = retrive_data("House_data",input)
-    result_addess = [i  for i in data]
-    return render_template('result.html',result=result_addess)
 
 
 
